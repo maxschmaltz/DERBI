6f94ea63aac70a20b591e587e305b73a2c6c8f67
maxschmaltz <85135022+maxschmaltz@users.noreply.github.com>
2022-02-16 02:49:15 +0300

    Update derbi.py

--- derbi.py
+++ derbi.py
@@ -94,7 +94,8 @@ class DERBI:
         for data in self.to_inflect.values():
             if data['target_tags'] == '<STOPTAG>':
                 data['result'] = data['token'].text.lower()
-            data['result'] = self.inflect(data['token'], data['target_tags'])
+            else:
+                data['result'] = self.inflect(data['token'], data['target_tags'])
         # assemble the result
         self.result_text = ' '.join([word.text if self.to_inflect.get(str(i)) is None else self.to_inflect[str(i)]['result'] 
                                                                                         for i, word in enumerate(self.doc)])
